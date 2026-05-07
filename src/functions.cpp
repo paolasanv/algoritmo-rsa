2c1ba38555e5bb5239da32a1fae4915a4640e370
JoshuaJc2 <joshua-j-c@ciencias.unam.mx>
2026-05-07 15:52:04 -0600

    Funciones mcd, mcde y potenciaMod implementadas

--- src/functions.cpp
+++ src/functions.cpp
@@ -6,12 +6,32 @@
 /** Eleva a a la b recortando con mod */
 uint64_t potenciaMod(uint64_t a, uint64_t b, uint64_t mod){
     uint64_t res = 1;
-
+    while(b >= 1){
+	    if(n%2 != 0){
+		    res = (res * a) % mod;
+		    b--;
+	    } else {
+		    a = (a * a) % mod;
+		    b /= 2;
+	    }
+    }
+    return res;
 }
 
 /**Encuentra la combinación lineal de a y b que resulta en 1. */
 int64_t mcde(uint64_t a, uint64_t b, int64_t &x, int64_t &y){
+	if(a == 0){
+		x = 0;
+		y = 1;
+		return b;
+	}
 
+	uint64_t x1, y1;
+	uint64_t mcd = mcde(b%a, a, x1, y1);
+
+	x = y1 - (b/a) * x1;
+	y = x1;
+	return mcd;
 }
 
 /** Encuentra el inverso multiplicativo módulo M. Es mejor obtenerlo
@@ -22,7 +42,10 @@ uint64_t invMult(uint64_t a, uint64_t mod){
 
 /** Encuentra el máximo común divisor entre a y b. */
 int64_t mcd(uint64_t a, uint64_t b){
-
+	if(a == 0){
+		return b;
+	}
+	return mcd(b%a, a);
 }
 
 /** Nos dice si un número es primo o no */
