0d69cbfb8c67395fa58ad8230376f108992a854e
Joshua Cruz <111657952+JoshuaJc2@users.noreply.github.com>
2026-05-08 10:52:48 -0600

    Constructores en Entidad.cpp

--- src/Entidad.cpp
+++ src/Entidad.cpp
@@ -5,19 +5,46 @@ uint64_t phiEuler(uint64_t n);
 int64_t mcde(uint64_t a, uint64_t b, int64_t &x, int64_t &y);
 int64_t mcd(uint64_t a, uint64_t b);
 uint64_t potenciaMod(uint64_t a, uint64_t b, uint64_t mod);
+uint64_t invMult(uint64_t a, uint64_t mod);
 uint rand(uint a, uint b);
 bool esPrimo(uint64_t a);
+bool coprimos(uint64_t a, uint64_t b);
 
 /** Construye una entidad usando dos números primos p y q */
 Entidad::Entidad(std::string nombre, uint64_t p, uint64_t q)
     :nombre{nombre} {
+        if(!esPrimo(p) || !esPrimo(q))
+		    throw std::invalid_argument("p y q deben ser primos.");
+	    if(p == q)
+	       throw std::invalid_argument("p y q debe ser distintos.");
 
+	    uint64_t n = p * q;
+	    uint64_t phi = (p-1)*(q-1);
+
+	    uint64_t e = 2;
+	    while(!coprimos(e,phi)){
+		    e++;
+	    }
+
+	    uint64_t d = invMult(e,phi);
+	    pub = {e,n};
+	    priv = {d,n};
 }
 
 /** Construye una entidad usando primos y una "semilla" e */
 Entidad::Entidad(std::string nombre, uint64_t p, uint64_t q, uint64_t e):
     nombre {nombre}{
+        if(!esPrimo(p) || !esPrimo(q))
+		    throw std::invalid_argument("p y q deben ser primos.");
+	    if(p == q)
+		    throw std::invalid_argument("p y q deben ser distintos.");
 
+	    uint64_t n = p * q;
+	    uint64_t phi = (p-1)*(q-1);
+
+	    uint64_t d = invMult(e,phi);
+	    pub = {e,n};
+	    priv = {d,n};
 }
 
 /** Agrega una llave externa a esta entidad */
